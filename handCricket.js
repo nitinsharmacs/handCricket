6e3e9732b95e72ad5618a563836963544777364f
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2022-04-27 11:08:53 +0530

    Implemented handCricket

---
+++ handCricket.js
@@ -0,0 +1,81 @@
+const winner = function (match) {
+  const batsMan = match.batsMan;
+  const bowler = match.bowler;
+  if (batsMan.runs > bowler.runs) {
+    return batsMan;
+  }
+  if (batsMan.runs < bowler.runs) {
+    return bowler;
+  }
+  if (batsMan.balls > bowler.balls) {
+    return bowler;
+  }
+  return batsMan;
+};
+
+const shakeHand = function () {
+  return Math.ceil(Math.random() * 6);
+};
+
+const isRunning = function (handSigns) {
+  return handSigns.batsMan !== handSigns.bowler;
+};
+
+const playInning = function () {
+  let runs = 0;
+  let balls = 0;
+  const handSigns = { batsMan: 0, bowler: 1 };
+  while (isRunning(handSigns)) {
+    runs += handSigns.batsMan;
+    balls += 1;
+    handSigns.batsMan = shakeHand();
+    handSigns.bowler = shakeHand();
+  }
+  return { balls: balls, runs: runs };
+};
+
+const updateBatsMan = function (match, inning) {
+  const batsMan = match.batsMan;
+  batsMan.runs = inning.runs;
+  batsMan.balls = inning.balls;
+  match.batsMan = batsMan;
+};
+
+const swapPlayers = function (match) {
+  const batsMan = match.batsMan;
+  match.batsMan = match.bowler;
+  match.bowler = batsMan;
+  return match;
+};
+
+const createMatch = function (players) {
+  return {
+    batsMan: {
+      name: players[0],
+    },
+    bowler: {
+      name: players[1],
+    },
+  };
+};
+
+const startMatch = function (players) {
+  const match = createMatch(players);
+  for (let index = 0; index < players.length; index++) {
+    let inning = playInning();
+    updateBatsMan(match, inning);
+    swapPlayers(match);
+  }
+  return {
+    match: match,
+    winner: winner(match)
+  };
+};
+
+const main = function () {
+  const matchResult = startMatch(['gayatri', 'sanjana']);
+  console.log(matchResult.match);
+  console.log(matchResult.winner);
+};
+
+main();
